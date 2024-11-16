f14e4dac9652dc06ab56d586c8bc0a7ff786d355
Sajjan Karna <karnaa787@gmail.com>
2024-11-16 23:13:09 +0545

    feat: add lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
   rules: {
+    "prefer-arrow-callback": ["error"],
+    "prefer-template": ["error"],
+    semi: ["error"],
+    quotes: ["error", "double"],
     "prettier/prettier": "error",
   },
 };
